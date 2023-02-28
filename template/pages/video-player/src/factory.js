04fb8eec7cb615fcb841afedf02032573e5952f5
David Bastos <davidtavares98@gmail.com>
2023-02-28 13:20:46 -0300

    add camera

--- template/pages/video-player/src/factory.js
+++ template/pages/video-player/src/factory.js
@@ -1,11 +1,14 @@
+import Camera from "../../../lib/shared/camera.js"
 import Controller from "./controller.js"
 import Service from "./service.js"
 import View from "./view.js"
 
 const [rootPath] = window.location.href.split('/pages/')
+const camera = await Camera.init()
 const factory = {
   async initialize() {
     return Controller.initialize({
+        camera,
         view: new View({}),
         service: new Service({})
     })
